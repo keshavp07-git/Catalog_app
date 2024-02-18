a4735200e230b97a37bad4d074ae662d6c5fadc2
keshavp07 <keshav.prajapati830@gmail.com>
2024-02-18 20:01:32 +0530

    Day2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 // ignore_for_file: prefer_const_constructors, unused_import
 import "package:flutter/material.dart";
+import "package:flutter_application_1/Home_Page.dart";
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyApp extends StatelessWidget { 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(child: Center(child: Container(child: Text("Hello World")))),
+      home:
+          HomePage(),
     );
   }
 }
